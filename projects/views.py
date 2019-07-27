5a711e4546d55adabf41b4d22c14624033a874af
theLife10 <jdgarcia9@miners.utep.edu>
2019-07-27 00:51:03 -0700

    backend almost finished

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-
+from projects.models import Project
 # Create your views here.
 
+def project_index(request):
+    projects = Project.objects.all()
+    context = {
+        'projects': projects
+    }
+    return render(request, 'project_index.html', context)
 
-def home_screen_view(request):
-	return render(request, "base.html", {})
+def project_detail(request, pk):
+    project = Project.objects.get(pk=pk)
+    context = {
+        'project': project
+    }
+    return render(request, 'project_detail.html', context)
